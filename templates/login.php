e57d954836bceae944fbc2999ea1b63b82b97995
Liosha <aleksej.jurkoit@gmail.com>
2017-10-26 14:35:26 +0300

    Login

---
+++ templates/login.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Dice Game</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/signin.css">
+
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+		<div class="container">
+		<h1 class="display-3">Dice Game!</h1>
+		<p class="lead">Simple dice game.</p>
+		</div>
+    </div>
+    <div class="container">
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Username</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <button type="submit" class="btn btn-primary btn-block" name="Login" value="Login">Lets play!</button>
+      </form>
+    </div> <!-- /container -->
