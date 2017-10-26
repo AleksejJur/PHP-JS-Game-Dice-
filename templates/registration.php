ebaa66df5aa961c6b8a89b489092837e77d01c02
Liosha <aleksej.jurkoit@gmail.com>
2017-10-26 14:08:36 +0300

    Basic
    Basic design on index and registration
    Registration (not working)

---
+++ templates/registration.php
@@ -0,0 +1,96 @@
+<?php
+
+session_start();
+require_once 'connect.php';
+
+if(isset($_POST['register'])){
+    
+    //Retrieve the field values from our registration form.
+    $name = !empty($_POST['name']) ? trim($_POST['name']) : null;
+    $email = !empty($_POST['email']) ? trim($_POST['email']) : null;
+    $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+    $pass = !empty($_POST['password']) ? trim($_POST['password']) : null;
+    
+    
+    //Construct the SQL statement and prepare it.
+    $sql = "SELECT COUNT(username) AS num FROM users WHERE username = :username";
+    $stmt = $pdo->prepare($sql);
+    
+
+    $stmt->bindValue(':username', $username);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    if($row['num'] > 0){
+        die('<div style="background-color:red;">That username already exists!</div>');
+    }
+    
+    //Hash the password as we do NOT want to store our passwords in plain text.
+    $passwordHash = password_hash($pass, PASSWORD_BCRYPT, array("cost" => 12));
+    
+    $sql = "INSERT INTO users (name, email, username, password) VALUES (:name, :email, :username, :password)";
+    $stmt = $pdo->prepare($sql);
+    
+    //Bind our variables.
+    $stmt->bindValue(':name', $name);
+    $stmt->bindValue(':email', $email);
+    $stmt->bindValue(':username', $username);
+    $stmt->bindValue(':password', $passwordHash);
+    $result = $stmt->execute();
+    if($result){
+        //What you do here is up to you!
+         echo '<div style="background-color: green;" class="text-center">Thank you for registering with our website.</div>';
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Registration</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <div class="container">
+      <form class="form-signin" method="POST">
+        <h2 class="form-signin-heading">Please register</h2>
+        <label for="inputName" class="sr-only">Name</label>
+        <input type="text" id="inputName" class="form-control" placeholder="Name" required autofocus>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputUsername" class="sr-only">Username</label>
+        <input type="text" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <br>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="register" value="register">Register</button>
+      </form>
+    </div> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
